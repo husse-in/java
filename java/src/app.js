53067a5f9f0779ef1984c295be2105eee2ec9424
husse-in <141724709+husse-in@users.noreply.github.com>
2023-09-20 00:53:17 +0400

    Add files via upload

---
+++ java/src/app.js
@@ -0,0 +1,19 @@
+//if(confirm("Do you want to come in?")) {
+  //  alert("Welcome to our site!");
+//} else
+  //   alert("Person not sure");
+    
+    
+ //  if(prompt("How old are you?")) {
+   // alert("What do want to know?");
+   //} else
+   //  alert("You are not 18 years,Sorry");
+       
+    var person = null;
+    if(confirm("Are you sure?")) {
+        peron = prompt("Enter your name");
+        person = prompt("Enter your surname");
+        person = prompt("Enter your password");
+    } else{
+        alert("You clicked cancel")
+    }
